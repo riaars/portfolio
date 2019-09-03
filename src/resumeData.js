505e19bf446d9d2d15d8290dc783c1be7ae56a46
Ria <riaratnasari@outlook.com>
2019-09-03 19:55:06 +0200

    edit resource

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,106 +1,139 @@
 let resumeData = {
-    "imagebaseurl":"https://rbhatia46.github.io/",
-    "name": "Rahul Bhatia",
-    "role": "Frontend Developer and Data Scientist",
-    "linkedinId":"Your LinkedIn Id",
-    "skypeid": "Your skypeid",
-    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
-    "socialLinks":[
-        {
-          "name":"linkedin",
-          "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
-          "className":"fa fa-linkedin"
-        },
-        {
-          "name":"github",
-          "url":"http://github.com/rbhatia46",
-          "className":"fa fa-github"
-        },
-        {
-          "name":"skype",
-          "url":"http://twitter.com/rbhatia46",
-          "className":"fa fa-twitter"
-        }
-      ],
-    "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
-    "address":"India",
-    "website":"https://rbhatia46.github.io",
-    "education":[
-      {
-        "UniversityName":"The LNM Insitute of Information Technology",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Aug",
-        "YearOfPassing":"2020",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "UniversityName":"Some University",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Jan",
-        "YearOfPassing":"2018",
-        "Achievements":"Some Achievements"
-      }
-    ],
-    "work":[
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      }
-    ],
-    "skillsDescription":"Your skills here",
-    "skills":[
-      {
-        "skillname":"HTML5"
-      },
-      {
-        "skillname":"CSS"
-      },
-      {
-        "skillname":"Reactjs"
-      }
-    ],
-    "portfolio":[
-      {
-        "name":"project1",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      },
-      {
-        "name":"project2",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/project.jpg"
-      },
-      {
-        "name":"project3",
-        "description":"mobileapp",  
-        "imgurl":"images/portfolio/project2.png"
-      },
-      {
-        "name":"project4",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      }
-    ],
-    "testimonials":[
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      },
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      }
-    ]
-  }
-  
-  export default resumeData
+  imagebaseurl: 'https://rbhatia46.github.io/',
+  name: 'Ria Ratna Sari',
+  role: 'UX Designer and Front End Developer',
+  linkedinId: 'https://www.linkedin.com/in/riaratnasari/',
+  skypeid: 'Your skypeid',
+  roleDescription:
+    'I like dabbling in various parts of frontend development and UX researchs. I like to learn about new technologies and blogging and traveling in my free time',
+  socialLinks: [
+    {
+      name: 'linkedin',
+      url: 'https://www.linkedin.com/in/riaratnasari/',
+      className: 'fa fa-linkedin',
+    },
+    {
+      name: 'github',
+      url: 'https://github.com/riaars',
+      className: 'fa fa-github',
+    },
+    {
+      name: 'twitter',
+      url: 'https://twitter.com/riaa_rs',
+      className: 'fa fa-twitter',
+    },
+  ],
+  aboutme:
+    'I am currently a final year master student at KTH Royal Institute of Technology, Sweden. I am taking Human-Computer Interaction and Design program',
+  address: 'Stockholm',
+  website: 'https://rbhatia46.github.io',
+  education: [
+    {
+      UniversityName: 'KTH Royal Institute of Technology',
+      specialization: 'Human-Computer Interaction and Design',
+      MonthOfPassing: 'June',
+      YearOfPassing: '2020',
+      Achievements: '',
+    },
+    {
+      UniversityName: 'Bandung Institute of Technology',
+      specialization: 'Electrical Engineering',
+      MonthOfPassing: 'July',
+      YearOfPassing: '2014',
+      Achievements: '',
+    },
+  ],
+  work: [
+    {
+      CompanyName: 'Bukalapak',
+      specialization: 'Associate Product Manager',
+      MonthOfLeaving: 'Aug',
+      YearOfLeaving: '2018',
+      Achievements: '',
+    },
+    {
+      CompanyName: 'Sale Stock Indonesia',
+      specialization: 'Analyst',
+      MonthOfLeaving: 'Aug',
+      YearOfLeaving: '2017',
+      Achievements: '',
+    },
+    {
+      CompanyName: 'Kartuku',
+      specialization: 'Software Engineer',
+      MonthOfLeaving: 'April',
+      YearOfLeaving: '2016',
+      Achievements: '',
+    },
+  ],
+  skillsDescription: 'Your skills here',
+  skills: [
+    {
+      skillname: 'HTML5',
+    },
+    {
+      skillname: 'CSS',
+    },
+    {
+      skillname: 'Reactjs',
+    },
+  ],
+  portfolio: [
+    {
+      name: 'BOOKIO',
+      description:
+        'BOOKIO is a leading book sharing platform that connects the people who want to give or lend their books away to the people who want to read a book without buying it. The app goal is to contribute to a sustainable environment while improving the world literacy',
+      imgurl: 'images/portfolio/books.jpg',
+    },
+    {
+      name: 'Shopistry',
+      description:
+        'Shopistry is a mobile platform that help mystery shoppers to perform the tasks onsite. The goal of the research is to create the app is to complete the task during visit without being suspicious',
+      imgurl: 'images/portfolio/shopping.jpg',
+    },
+    {
+      name: 'Food Sharer',
+      description:
+        'FoodSharer is a propose solution platform that will ebale the connect ness the food bank staff with the charity organization in Sweden. ',
+      imgurl: 'images/portfolio/judah.jpg',
+    },
+    {
+      name: 'Harvest Haven',
+      description:
+        'Harvest Haven is agri-tourism platform that allows the farmers to advertise their farming experiences  to farming tourist enthusiasts.',
+      imgurl: 'images/portfolio/origami.jpg',
+    },
+    {
+      name: 'project5',
+      description: 'mobileapp',
+      imgurl: 'images/portfolio/origami.jpg',
+    },
+    {
+      name: 'project6',
+      description: 'mobileapp',
+      imgurl: 'images/portfolio/origami.jpg',
+    },
+    {
+      name: 'project7',
+      description: 'mobileapp',
+      imgurl: 'images/portfolio/origami.jpg',
+    },
+    {
+      name: 'project7',
+      description: 'mobileapp',
+      imgurl: 'images/portfolio/origami.jpg',
+    },
+  ],
+  testimonials: [
+    {
+      description: 'This is a sample testimonial',
+      name: 'Some technical guy',
+    },
+    {
+      description: 'This is a sample testimonial',
+      name: 'Some technical guy',
+    },
+  ],
+};
+
+export default resumeData;
